971354917372df5ef8897e7d593b4066c5ff5bde
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-05-15 00:26:03 +0200

    update poligon to use new centrala module

--- scripts/poligon.py
+++ scripts/poligon.py
@@ -1,13 +1,14 @@
 import os
-from typing import Any
 
 import requests
 from dotenv import load_dotenv
 
+from utils.centrala import Centrala
+
 load_dotenv()
 
 TASK_NAME = "POLIGON"
-AIDEVS_API_KEY = os.getenv("AIDEVS_API_KEY")
+AIDEVS_API_KEY = os.environ["AIDEVS_API_KEY"]
 
 
 def get_input() -> str:
@@ -16,21 +17,18 @@ def get_input() -> str:
     return response.text
 
 
-def send_solution(solution: str | list[Any]):
-    payload = {
-        "task": TASK_NAME,
-        "apikey": AIDEVS_API_KEY,
-        "answer": solution,
-    }
-    response = requests.post("https://poligon.aidevs.pl/verify", json=payload)
-    response.raise_for_status()
-    return response.text
+def main():
+    if not AIDEVS_API_KEY:
+        raise ValueError("AIDEVS_API_KEY not found in environment variables.")
+
+    input_data = get_input()
+    solution_list = [s for s in input_data.split("\n") if s]
+    print(f"Solution prepared: {solution_list}")
+
+    centrala_client = Centrala(task_name=TASK_NAME, apikey=AIDEVS_API_KEY)
+    output = centrala_client.send_solution(solution_list)
+    print(f"Output from AiDevs: {output}")
 
 
 if __name__ == "__main__":
-    input_data = get_input()
-    solution = input_data.split("\n")
-    solution = [s for s in solution if s]
-    print(solution)
-    output = send_solution(solution)
-    print(output)
+    main()
